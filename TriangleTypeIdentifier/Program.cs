b6cc4bfd8bb6b50c5ee3e73de6ccd344034a1aa1
Yasmin Morrison <morrisonismailayasmin@gmail.com>
2025-08-03 02:26:15 +0000

    Completed Triangle identifier system

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter side 1: ");
+        double side1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side 2: ");
+        double side2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side 3: ");
+        double side3 = double.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+            Console.WriteLine("Triangle Type: Equilateral");
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
+            Console.WriteLine("Triangle Type: Isosceles");
+        else
+            Console.WriteLine("Triangle Type: Scalene");
+
+        Console.ReadLine();
+    }
+}
