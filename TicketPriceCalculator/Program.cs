64c6f03fe66aa7718d82b4dbdcf38282843a8fb7
Yasmin Morrison <morrisonismailayasmin@gmail.com>
2025-08-03 02:25:03 +0000

    Completed the ticket calculator system

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        double price = (age <= 12 || age >= 65) ? 7.0 : 10.0;
+
+        Console.WriteLine($"Ticket Price: GHC{price}");
+
+        Console.ReadLine();
+    }
+}
