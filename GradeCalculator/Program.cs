ca6759653ad3632c8cfe0d0ca7ef40652915011d
Yasmin Morrison <morrisonismailayasmin@gmail.com>
2025-08-03 02:23:51 +0000

    Added Grade calculator system

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a grade (0-100): ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+            Console.WriteLine("Letter Grade: A");
+        else if (grade >= 80)
+            Console.WriteLine("Letter Grade: B");
+        else if (grade >= 70)
+            Console.WriteLine("Letter Grade: C");
+        else if (grade >= 60)
+            Console.WriteLine("Letter Grade: D");
+        else
+            Console.WriteLine("Letter Grade: F");
+
+        Console.ReadLine();
+    }
+}
